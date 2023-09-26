ad08b73204cdeea78b0b19e9e070ccc5750ea47e
Miguel Gs <miguelgsantos98@gmail.com>
2023-09-26 14:29:21 -0300

    criando domain com cliente, adcionando referencias ao entityFrameWork e mapeando domain como dbset

---
+++ AR.Domain/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AR.Domain
+{
+    public class Cliente
+    {
+        [Key]
+        public Guid id { get; set; }
+
+        [Required(ErrorMessage = " O campo {0} é obrigatório!")]
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public int Idade { get; set; }
+
+    }
+}
